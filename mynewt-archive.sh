c9a7f2d2c4cec7d9c9281c3f4c30ef89865747a9
Christopher Collins <ccollins476ad@gmail.com>
2016-03-04 18:11:20 -0800

    Tool for preparing  mynewt source releases.

---
+++ mynewt-archive.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+set -e
+
+print_usage() {
+    echo "usage: $(basename "$0") <tagname> <dst-prefix>"
+    echo "  example: $(basename "$0")  mynewt_0_8_0_b2_tag ~/rel/apache-larva-0.8.0-incubating-b2"
+}
+
+usage_err() {
+    if [ "$1" != "" ]
+    then
+        printf "* error: %s\n" >&2
+    fi
+
+    print_usage >&2
+}
+
+tagname=$1
+dstprefix=$2
+
+if [ "$tagname" = '' ] || [ "$dstprefix" = '' ]
+then
+    usage_err
+    exit 1
+fi
+
+dstfile="$dstprefix.tgz"
+tarprefix=$(basename "$dstprefix")
+
+# Create tgz file.
+git archive --format tgz --output "$dstfile" --prefix "$tarprefix"/ "$tagname"
+
+# Create ASCII armored detached signature.
+gpg2 --armor --output "$dstfile".asc --detach-sig "$dstfile"
+
+# Create sha; cd to target directory first for friendlier output.
+(
+    dir="$(dirname "$dstfile")"
+    filename="$(basename "$dstfile")"
+    cd "$dir" &&
+    gpg2 --print-md SHA512 "$filename" > "$filename".sha
+)
